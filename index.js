63ecd2dba4249ab8cd964f0fc732d6936dac4ccf
maslowind <bart1885@gmail.com>
2020-04-01 20:18:49 +0300

    Demo without tests

--- index.js
+++ index.js
@@ -7,23 +7,24 @@ const fs = require('fs');
 
 app.get('/', function (req, res) {
     const data = jsonHelper.getJsonData();
-    const location = data.location;
+    const format = data.format;
     const language = data.language;
     const text = data.text;
-    let price=0, time=0;
-    countHelper.getTextLength(location, text, function (countOfSymbols) {
-        console.log(countOfSymbols);
-        price = countHelper.getPrice(countOfSymbols, language);
-        time = countHelper.getTime(countOfSymbols, language);
-        if(!countHelper.isExpensive(location,text)) {
-            price = 1.2 * price;
-            time = 1.2 * time;
-            console.log(countHelper.isExpensive(location,text))
-        }
-        console.log(price, time); 
-        countHelper.isExpensive(location);       
-    })
-    res.send("OK");
+
+    let price = 0, time = 0;
+    let countOfSymbols = text.length;
+    price = countHelper.getPrice(countOfSymbols, language);
+    time = countHelper.getTime(countOfSymbols, language);
+    
+    if (!countHelper.isExpensive(format)) {
+        price = 1.2 * price;
+        time = 1.2 * time;
+    }
+    
+    countHelper.output(price, time)
+    console.log(countHelper.output(price, time))
+    
+    res.send(countHelper.output(price, time));
 });
 
 
--- services/countHelper.js
+++ services/countHelper.js
@@ -1,38 +1,116 @@
-const textract = require('textract');
-
-exports.getTextLength = function (location, text, callback) {
-  if (text === "") {
-    textract.fromFileWithPath(location, function (error, content) {
-      callback(content.length);
-    })
-  }
-  else {
-    callback(text.length);
-  }
-};
-
-exports.getPrice = function (countOfSymbols, language) {
+exports.getPrice = function (countOfSymbols, language) { //ф-ция для определения стоимости
   let priceForSymbol, price;
   if (language === "ru" || language === "ua") priceForSymbol = 0.05;
   else if (language === "en") priceForSymbol = 0.12;
+  else return 0;
   if (countOfSymbols < 1000) price = 1000 * priceForSymbol;
   else price = countOfSymbols * priceForSymbol;
   return price;
 };
 
-exports.getTime = function (countOfSymbols, language) {
+
+
+
+
+exports.getTime = function (countOfSymbols, language) { //ф-ция для определения затраченного времени
   let timeForSymbol, time;
   if (language === "ru" || language === "ua") timeForSymbol = 1333;
   else if (language === "en") timeForSymbol = 333;
+  else return 0;
   time = 0.5 + countOfSymbols / timeForSymbol;
   if (time < 1) time = 1;
   return time;
 };
 
-exports.isExpensive = function (location, text) {
-  let format = location.split(".").pop();
-  if (format === "docx" || format === "doc" || format === "rtf" || text != "") return true;
-  else return false;
 
+
+
+
+exports.isExpensive = function (format) { // ф-ция для проверки формата файла
+  if (format === "docx" || format === "doc" || format === "rtf") return true;
+  else return false;
 };
 
+
+
+
+
+
+const slavicGetDay = function (numb) { // ф-ция для перевода дней недели в "славянскую" систему координат
+  let weekdays = [6, 0, 1, 2, 3, 4, 5]
+  return weekdays[numb];
+}
+
+
+
+
+
+
+
+const getfinalDate = function (allTimeInHours) { //ф-ция для подсчета даты, когда продукт будет готов
+  let countOfHolidays = 0; // кол-во оставшихся до понедельника выходных дней с момента заказа
+  let countOfWeeks = 0; //кол-во недель, которые уйдут на реализацию заказа
+  let countOfWorkingDays = 0; // кол-во  дней, которые уйдут на реализацию заказа с учетом выходных
+  let todayDate = new Date(); // сегодняшняя дата
+
+  let timeInDays = Math.floor(allTimeInHours / 9); // кол-во рабочих дней, которые уйдут на реализацию заказа 
+
+  let timeInHoursPerDay = convertDaysToMs(0, allTimeInHours % 9, 0, 0, 0); //кол-во мс, которое понадобится в последний день, чтоб выполнить заказ
+  let todayDateHourMs = convertDaysToMs(0, todayDate.getHours(), todayDate.getMinutes(), 0, 0); // время когда заказ сделан в мс
+  let resultTimeInMs = 0; //Время дня, когда заказ будет готов
+  if (todayDateHourMs < convertDaysToMs(0, 9, 0, 0, 0) || slavicGetDay(todayDate.getDay()) >= 5) {// Случай когда сделан раньше 9.00 АМ или в выходной день
+    resultTimeInMs = convertDaysToMs(0, 9, 0, 0, 0) + timeInHoursPerDay;
+  }
+  else if (todayDateHourMs > convertDaysToMs(0, 19, 0, 0, 0)) { // Случай когда сделан позже 7.00 РМ 
+    resultTimeInMs = convertDaysToMs(0, 9, 0, 0, 0) + timeInHoursPerDay;
+    timeInDays++;
+  }
+  else if ((todayDateHourMs + timeInHoursPerDay) > convertDaysToMs(0, 19, 0, 0, 0)) { // Случай когда заказ выполняется позже 7.00 РМ 
+    resultTimeInMs = todayDateHourMs + timeInHoursPerDay - convertDaysToMs(0, 10, 0, 0, 0);
+    timeInDays++;
+  }
+  else { //Заказ сделан в период с 9.00 АМ до 7.00 РМ в будний день
+    resultTimeInMs = todayDateHourMs + timeInHoursPerDay;
+  }
+
+
+  if ((timeInDays + slavicGetDay(todayDate.getDay()) > 5)) { // в данном блоке получаем кол-во недель, которые потребуются на реализацию заказа
+    if (slavicGetDay(todayDate.getDay()) >= 5) { // в данном блоке обрабатываем случай, если заказ сделан в выходной день
+      countOfWeeks = Math.floor(timeInDays / 5);
+      countOfHolidays = 7 - slavicGetDay(todayDate.getDay());
+    }
+    else { // в данном блоке обрабатываем случай, если заказ сделан в рабочий день
+      countOfWeeks = Math.floor((timeInDays + slavicGetDay(todayDate.getDay())) / 5);
+    }
+  }
+
+  countOfWorkingDays = countOfHolidays + countOfWeeks * 2 + timeInDays; // получаем кол-во  дней, которые уйдут на реализацию заказа с учетом выходных
+
+  let dateWithDayMs = todayDate.getTime() + convertDaysToMs(countOfWorkingDays, 0, 0, 0, 0) + resultTimeInMs - todayDateHourMs;
+  let dateWithDay = new Date(dateWithDayMs);
+  return dateWithDay;
+};
+
+
+
+
+const outputHelper = function (num) {//функция для красоты вывода
+  if (num < 10) return "0" + num;
+  else return num;
+}
+
+
+
+
+const convertDaysToMs = function (days, hours, min, sec, ms) {// конвертер из дней в мс
+  return (((days * 24 + hours) * 60 + min) * 60 + sec) * 1000 + ms;
+}
+
+exports.output = function (price, time) { //вывод
+  let finalDate = getfinalDate(time);
+  let result = "Спасибо! Стоимость Вашего заказа: " +
+    Math.round(price, 0) + " грн. Вы можете забрать его в " + outputHelper(finalDate.getHours()) + ":" +
+    outputHelper(finalDate.getMinutes()) + " " + outputHelper(finalDate.getDate()) + "." + outputHelper(finalDate.getMonth() + 1)
+    + "." + finalDate.getFullYear();
+  return result;
+}
